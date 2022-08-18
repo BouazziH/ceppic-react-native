f06cf1ba7e669815b72f604c9d588410378d28d1
BouazziH <bouazzicdi@gmail.com>
2022-08-18 14:14:34 +0200

    ustilisation des usestates et les evenements input,button et les function

--- App.js
+++ App.js
@@ -1,13 +1,27 @@
 
 import { StyleSheet, Text, View , TextInput ,Button } from 'react-native';
-
+import { useState } from 'react';
 
 export default function App() {
+  //on utilise le usestate pour recuper les valeurs des champs
+  //on declare des variable tableaux  vide pour les remplires
+  const [lienTextSaisie, setLienTextSaisie] = useState('');
+  const [ListeLien, setListeLien] = useState([]);
+  function ajoutLienHandler(){
+    console.log(lienTextSaisie);
+   
+  }
+  function lienInputHandler(textsaisie){
+    //affecter la valeur entrer au sette
+    setLienTextSaisie(textsaisie);
+    //console.log(textsaisie);
+  }
   return (
     <View style={styles.container}>
       <View style={styles.inputContainer}>
-      <TextInput defaultValue='Nouveau lien.. ' style={styles.inputLien}/>
-<Button title='Ajouter un lien' />
+      <TextInput placeholder='Nouveau lien.. ' style={styles.inputLien} onChangeText={lienInputHandler}/>
+<Button title='Ajouter un lien'
+ onPress={ajoutLienHandler} />
 </View>
       <View style={styles.lienContainer}>
         <Text>Liste des liens ...</Text>
@@ -27,12 +41,15 @@ const styles = StyleSheet.create({
     color: 'blue',
     borderColor: "gray",
     borderWidth: 1,
-    padding: 8,
+    padding: 8
   },
   inputContainer:{
-  
+    flex:1,
     flexDirection:'row',
     alignItems:"center",
-  
+    
+  },
+  lienContainer:{
+    flex:3,
   }
 });
