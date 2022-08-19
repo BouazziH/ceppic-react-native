9fd46ccf0bff56d70dc2126fb91bd7d20551b735
BouazziH <bouazzicdi@gmail.com>
2022-08-19 10:05:37 +0200

    creation de repertoire  composant ajout du lienItem avec son style

---
+++ components/LienItem.js
@@ -0,0 +1,23 @@
+import { StyleSheet,View ,Text } from "react-native-web"
+
+export default function LienItem(props) {
+    
+    return(
+<View style={styles.lienItem}>
+  <Text style={styles.lienText}>{props.text}</Text>
+</View>
+    )
+
+};
+ const styles =StyleSheet.create({
+
+    lienItem: {
+        backgroundColor:"#edcedc",
+        marginTop:20,
+        padding:8,
+        color:"#ggg",
+      },
+      lienText:{
+        color:"yellow",
+      }
+ });
