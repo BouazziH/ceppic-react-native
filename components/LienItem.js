6c54f0de2d1a6fd28fb8565df108d1c61a6f46da
BouazziH <bouazzicdi@gmail.com>
2022-08-19 14:11:24 +0200

    utilisation de pressable et ces methode pour faire supprimer un lien

--- components/LienItem.js
+++ components/LienItem.js
@@ -1,11 +1,18 @@
+import { Pressable } from "react-native";
 import { StyleSheet,View ,Text } from "react-native-web"
 
 export default function LienItem(props) {
+   
+   console.log(props.id);
+   
     // ajour du props
     return(
+        //pressable est un coposant reacte pour le click(selectionner) sur un lien/on utlise le props + methode bind to delete the item
+        <Pressable onPress={props.onDeleteItem.bind(this,props.id)}>
 <View style={styles.lienItem}>
   <Text style={styles.lienText}>{props.text}</Text>
 </View>
+</Pressable>
     )
 
 };
